ca57db46a0a39074523ef9c1ff121747992e3dbd
Tuhirirwe-Maria <tuhirirwemaria@gmail.com>
2021-09-06 11:53:20 +0300

    add Order Page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Order from './Components/Orders'
+
+function App() {
+  return (
+    <div className="App">
+      <Order/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Orders.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './OrderPage/Header'
+import Section from './OrderPage/Section'
+
+const Orders = () => {
+    return (
+        <div>
+            <Header/>
+            <Section/>
+        </div>
+    )
+}
+
+export default Orders
