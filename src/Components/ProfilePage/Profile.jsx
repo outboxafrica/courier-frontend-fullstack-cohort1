666375e25b3dedead3e0e97c6161bb73bf8c9c4a
5faris <ebetpharys@gmail.com>
2021-09-06 13:06:39 +0300

    see profile page

---
+++ src/Components/ProfilePage/Profile.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import '../Profile.css';
+
+
+ const Profile = () => {
+    return (
+        <div className="profile__card">
+            <div className="img__profile">
+                <img src="../Images/profile.png" alt="profile" /> 
+        </div>
+ 
+        <div className="user__name">
+                <input 
+         className="input" type="text" placeholder="Name" 
+          />
+            </div>
+            <div className="user__email">
+                <input 
+         className="input" type="text" placeholder="Email"  
+          />
+            </div>
+            <div>
+            <button className="signin__button" type="button">Sign in</button>
+            </div>
+        <hr></hr>
+            <div>
+            
+            <span className="profile__accountLink">Add another account</span>
+        </div>
+        </div>
+    )
+}
+
+export default Profile;
